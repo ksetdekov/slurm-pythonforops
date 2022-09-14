56d3b6fd2532cffdf19f6332b692b7501e8811b9
ksetdekov <ksetdekov@gmail.com>
2022-09-15 01:40:16 +0300

    done hw

---
+++ 03 Оптимизация и ооп/03_03_05_homework.py
@@ -0,0 +1,41 @@
+import json
+from collections import deque
+
+
+def extract_int(string):
+    """из строки  в формате как в задании вытаскивает число
+
+    :param string: строка с именем прокси
+    :type string: str
+    :return: число номера
+    :rtype: int
+    """    
+    value = int(string.split("t")[1].split(".")[0])
+    return value
+
+
+def main():
+    proxys = input()
+    proxys = json.loads(proxys)
+    # convert to ints
+    proxys = [extract_int(i) for i in proxys]
+    proxys_que = deque(proxys)
+    print(proxys_que)
+    print(len(proxys_que))
+
+    # cycle
+    for _ in range(1000):
+        candidate = proxys_que.pop()
+        print(f'Обращение при помощи прокси proxyhost{candidate}.slurm.io')
+
+        if candidate % 3 == 0 or candidate % 8 == 0:
+            continue
+        print(
+            f'Было осуществлено обращение к ресурсу при помощи прокси "proxyhost{candidate}.slurm.io"')
+        proxys_que.appendleft(candidate)
+    else:
+        print(f'числ оставшихся в списке прокси {len(proxys_que)}')
+
+
+if __name__ == '__main__':
+    main()
