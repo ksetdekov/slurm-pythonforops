2850da449fffdd8302137662f1e10813ebdabad9
Сетдеков Кирилл <KRSetdekov@sberbank.ru>
2022-09-21 00:46:15 +0300

    доделал дз

--- 03 Оптимизация и ооп/03_04_18_homework.py
+++ 03 Оптимизация и ооп/03_04_18_homework.py
@@ -21,11 +21,14 @@
 # На отрицательные значения налагаются ограничения - 
 # в случае их указания программа должна бросать исключение ValueError.
 
-# Код из этого задания отправлять на проверку не нужно, 
-# в следующих уроках будут задания, связанные с его доработкой.
+# Теперь давайте создадим специализации классов агента сбора метрик для Prometheus и Carbon.
+#
+# Агент для Prometheus не имеет дополнений к родительским полям. Но не позволяет управлять периодом отправки событий,
+# т.к. работает в модели pull.
 import re
 
-class MetricCollector():
+
+class MetricCollector:
     def __init__(self, settings_dict) -> None:
         # Агент должен иметь IP адрес сервера, к которому подключается, 
         # хранить ключ доступа к серверу и 
@@ -36,12 +39,12 @@ class MetricCollector():
         self.__collection_frequency = settings_dict['collection_frequency']
         self.__send_frequency = settings_dict['send_frequency']
         self.collect_counter = 0
-    
+
     def __str__(self) -> str:
-        list_of_attributes = [self.ip_address, self.server_key, self.__collection_frequency, self.__send_frequency, self.collect_counter]
+        list_of_attributes = [self.ip_address, self.server_key, self.__collection_frequency, self.__send_frequency,
+                              self.collect_counter]
         list_of_strings = [str(value) for value in list_of_attributes]
         return ", ".join(list_of_strings)
-
 
     @staticmethod
     def process_times(timestring):
@@ -63,9 +66,8 @@ class MetricCollector():
             s_match = re.search(r"[\d]*?(?=s)", timestring)
             if s_match is not None:
                 time_seconds += int(s_match[0])
-            return  time_seconds
+            return time_seconds
 
-    
     @property
     def collection_frequency(self):
         return self.__collection_frequency
@@ -81,29 +83,55 @@ class MetricCollector():
     @send_frequency.setter
     def send_frequency(self, new_value):
         self.__send_frequency = MetricCollector.process_times(new_value)
-        
+
     def get_events(self):
-    # Собрать события: при его использовании должно выводиться сообщение "события сервера <IP-адрес> собраны. Следующий сбор через <период сбора метрик> секунд".
+        # Собрать события: при его использовании должно выводиться сообщение
+        # "события сервера <IP-адрес> собраны. Следующий сбор через <период сбора метрик> секунд".
         self.collect_counter += 1
         print(f"события сервера {self.ip_address} собраны. Следующий сбор через {self.__collection_frequency} секунд")
 
     def send_events(self):
-    # Отправка событий на сервер сбора метрик: выводится сообщение "события сервера <IP-адрес> собраны, отправлены на сервер сбора метрик. Следующая отправка через <период отправки событий на сервер сбора событий> секунд".
+        # Отправка событий на сервер сбора метрик: выводится сообщение "события сервера <IP-адрес> собраны, отправлены
+        # на сервер сбора метрик. Следующая отправка через <период отправки событий на сервер сбора событий> секунд".
         self.collect_counter += 1
-        print(f"события сервера {self.ip_address} собраны, отправлены на сервер сбора метрик. Следующая отправка через {self.__send_frequency} секунд")
-    
+        print(f"события сервера {self.ip_address} собраны, "
+              f"отправлены на сервер сбора метрик. Следующая отправка через {self.__send_frequency} секунд")
+
     def clear_cache(self):
-    # Обнуление кеша агента: выводится сообщение "кеш агента был очищен" и  число вызовов метода сборки событий сбрасывается на 0.
+        # Обнуление кеша агента: выводится сообщение "кеш агента был очищен"
+        # и число вызовов метода сборки событий сбрасывается на 0.
         self.collect_counter = 0
         print("кеш агента был очищен")
 
     def get_event_count(self):
-    # Получение информации о числе собранных событий: выводится сообщение "С сервера <IP-адрес> собрано <число вызовов метода сборки событий> событий".
+        # Получение информации о числе собранных событий: выводится сообщение
+        # "С сервера <IP-адрес> собрано <число вызовов метода сборки событий> событий".
         print(f"С сервера {self.ip_address} собрано {self.collect_counter} событий")
 
 
+class PrometheusAgent(MetricCollector):
+    def __init__(self, settings_dict):
+        super().__init__(settings_dict)
+
+    def send_events(self):
+        self.collect_counter += 1
+        print(f"события сервера {self.ip_address} собраны, отправлены по запросу от Prometheus")
+
+
+class CarbonAgent(MetricCollector):
+    def __init__(self, settings_dict):
+        super().__init__(settings_dict)
+
+    def send_events(self):
+        self.collect_counter += 1
+        print(f"события сервера {self.ip_address} собраны, отправлены в Carbon. "
+              f"Следующая отправка через {self.send_frequency} секунд")
+
+
 def main():
-    metric_server = MetricCollector(settings_dict={'ip_address':'1.1.1.1', 'server_key':'fjkdsflsdfjdslf', 'collection_frequency':17, 'send_frequency':11})
+    base_settings = {'ip_address': '1.1.1.1', 'server_key': 'fjkdsflsdfjdslf', 'collection_frequency': 17,
+                     'send_frequency': 11}
+    metric_server = MetricCollector(settings_dict=base_settings)
     print(metric_server)
     metric_server.get_events()
     metric_server.get_events()
@@ -118,6 +146,15 @@ def main():
     metric_server.send_events()
     metric_server.get_event_count()
     print(metric_server)
+
+    print("проверка что у всех экзэмляров разных классов работает метод send_events")
+
+    regular_collector = MetricCollector(base_settings)
+    carbon_collector = PrometheusAgent(base_settings)
+    prometheus_collector = CarbonAgent(base_settings)
+
+    for agent in [regular_collector, carbon_collector, prometheus_collector]:
+        agent.send_events()
 
 
 if __name__ == '__main__':
