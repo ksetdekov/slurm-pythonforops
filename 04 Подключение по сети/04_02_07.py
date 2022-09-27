c4b4c82ddc175fae893adbc5aa727b81f604e7b4
Сетдеков Кирилл <KRSetdekov@sberbank.ru>
2022-09-27 23:48:47 +0300

    добавил код лекции

--- 04 Подключение по сети/04_02_07.py
+++ 04 Подключение по сети/04_02_07.py
@@ -1,9 +1,51 @@
+import json
 import requests
 def main():
+    session = requests.Session()
     base_url = "https://httpbin.org"
 
-    response = requests.get(base_url +"/get")
-    print(response)
+    query_params = {
+        "param1": "foo",
+        "param2": "bar"
+    }
+
+    headers = {
+        "User-Agent": "Chrome",
+        "Authorization": "Bearer: sfsdfsfd"
+    }
+
+    payload = {
+        "id": 12313,
+        "name": "python"
+    }
+    
+    with open("README.md") as text_file:
+        files = {
+            "text_file": text_file
+        }
+
+    # response = requests.get(base_url +"/get", params=query_parms, headers=headers)
+        try:
+            response = session.post(url=base_url + "/post",
+                                    params=query_params,
+                                    headers=headers,
+                                    data=payload,
+                                    files=files,
+                                    timeout=3)
+
+            response_2 = session.get(url=base_url + "/get",
+                                     params=query_params,
+                                     headers=headers,
+                                     timeout=0.01)
+            print(response.status_code)
+            print(response.text)
+            jsonified_response_2 = json.loads(response_2.text)
+            print(jsonified_response_2)
+        except requests.exceptions.ConnectTimeout:
+            print("Ресурс недоступен")
+
+
+
 
 if __name__ == '__main__':
     main()
