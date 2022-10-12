2649ccbd15d5d96dadc2a7fbe461b5e379ee53cc
Сетдеков Кирилл <KRSetdekov@sberbank.ru>
2022-10-12 10:11:44 +0300

    скелет модуля готов

--- 08_ansible_module/clickhouse.py
+++ 08_ansible_module/clickhouse.py
@@ -24,93 +24,59 @@ author:
 EXAMPLES = r'''
 # Pass in a message
 - name: Connect to DBMS clickhouse and create user
+    clickhouse:
+        login_user: default
+        login_password: default
+        user: new username
+        password:  new user's password
 
 
-# pass in a message and have changed true
-- name: Test with a message and changed output
-  my_namespace.my_collection.my_test:
-    name: hello world
-    new: true
-
-# fail the module
-- name: Test failure of the module
-  my_namespace.my_collection.my_test:
-    name: fail me
+- name: Connect to DBMS clickhouse and delete user
+    clickhouse:
+        login_user: default
+        login_password: default
+        user: deleted username
+        password:  absent
 '''
 
 RETURN = r'''
 # These are examples of possible return values, and in general should use other names for return values.
-original_message:
-    description: The original name param that was passed in.
-    type: str
+mutations:
+    description: List of executed mutation querries
     returned: always
-    sample: 'hello world'
-message:
-    description: The output message that the test module generates.
-    type: str
-    returned: always
-    sample: 'goodbye'
+    type: list
+    sample: ['CREATE USER %(new_user)s {"new_user": "john"}']
+    version: '2.8'
 '''
 
 from ansible.module_utils.basic import AnsibleModule
 
 
-def run_module():
-    # define available arguments/parameters a user can pass to the module
-    module_args = dict(
-        name=dict(type='str', required=True),
-        new=dict(type='bool', required=False, default=False)
-    )
+def main():
 
-    # seed the result dict in the object
-    # we primarily care about changed and state
-    # changed is if this module effectively modified the target
-    # state will include any data that you want your module to pass back
-    # for consumption, for example, in a subsequent task
-    result = dict(
-        changed=False,
-        original_message='',
-        message=''
-    )
+    module_args = {
+        "login_user": {"type":"str", "required":True},
+        "login_password": {"type":"str", "required":True},
+        "user": {"type":"str", "required":True},
+        "password": {"type":"str", "required":False},
+        "state": {"type":"str", "required":False, "default": "new"}
+    }
 
-    # the AnsibleModule object will be our abstraction working with Ansible
-    # this includes instantiation, a couple of common attr would be the
-    # args/params passed to the execution, as well as if the module
-    # supports check mode
+    result = {
+        "changed":False
+    }
+
     module = AnsibleModule(
         argument_spec=module_args,
         supports_check_mode=True
     )
 
-    # if the user is working with this module in only check mode we do not
-    # want to make any changes to the environment, just return the current
-    # state with no modifications
     if module.check_mode:
         module.exit_json(**result)
 
-    # manipulate or modify the state as needed (this is going to be the
-    # part where your module will do what it needs to do)
-    result['original_message'] = module.params['name']
-    result['message'] = 'goodbye'
-
-    # use whatever logic you need to determine whether or not this module
-    # made any modifications to your target
-    if module.params['new']:
-        result['changed'] = True
-
-    # during the execution of the module, if there is an exception or a
-    # conditional state that effectively causes a failure, run
-    # AnsibleModule.fail_json() to pass in the message and the result
-    if module.params['name'] == 'fail me':
-        module.fail_json(msg='You requested this to fail', **result)
-
-    # in the event of a successful module execution, you will want to
-    # simple AnsibleModule.exit_json(), passing the key/value results
     module.exit_json(**result)
 
 
-def main():
-    run_module()
 
 
 if __name__ == '__main__':
